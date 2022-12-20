ef697f211df6ac96a538ad79dfb4b60c3ed6df13
Aryan Dev <103744719+Aaryan-Dev@users.noreply.github.com>
2022-12-20 15:00:07 +0530

    Update index.js

--- masai_game/index.js
+++ masai_game/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("MASAI WORD GAME");
+  res.send("MASAI WORD GAME, /adduser, /plazone, getuser");
 });
 
 app.post("/adduser", async (req, res) => {
